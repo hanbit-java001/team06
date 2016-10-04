bc6fdd2b3d0948b0922b1fc2dcb1a9bd0676d58a
hb5027 <hb5027@hb5027-PC>
2016-10-04 15:48:55 +0900

    - 테이블 (1)member (2)photo 생성SQL 업데이트 등 - build.gradle 업데이트

--- sql/Worksheet 1.sql
+++
@@ -1,8 +0,0 @@
-SELECT SEQ_PHOTO.CURRVAL FROM DUAL;
-----------------------------------------------------------
-SELECT * FROM PHOTO;
-----------------------------------------------------------
-INSERT INTO PHOTO (IMAGE_ID, IMAGE_NAME,IMAGE_REGDATE)
- VALUES (SEQ_PHOTO.NEXTVAL, 'aaa2.jpg', TO_CHAR(SYSDATE,'YYYYMMDDHH24MISS'));
-----------------------------------------------------------
-COMMIT;
