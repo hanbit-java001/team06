eb33e11a408e0979c5c4bac4873a2180056efd17
1111 <eocndtkfdj4@naver.com>
2016-11-03 14:07:42 +0900

    16/11/03 update

---
+++ src/main/java/com/hanbit/team06/web/controller/NaverLoginController.java
@@ -0,0 +1,23 @@
+package com.hanbit.team06.web.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.hanbit.team06.core.session.LoginRequired;
+
+@Controller
+public class NaverLoginController {
+
+	@RequestMapping("/naverIdLogin")
+	public String welcome() {
+
+		return "naverIdLogin";
+	}
+
+
+}
